883c0bb7446ffa254de366dc8eea618a00cb8758
martin <martin@borho.net>
2009-06-04 14:46:44 +0200

    added example plugin

---
+++ baas/plugins/example.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from baas.core.plugins import Plugin
+
+class Example(Plugin):
+
+    def get_map(self):
+        """
+            returns the command map for the plugin
+        """
+        cmd_map = [('example',self.example_action)]
+        return cmd_map
+
+    def get_help(self):
+        """
+            returns the help text for the plugin
+        """
+        return {'commands':['example:foo - gives you a bla for a foo'],'additional':[]}
+
+    def example_action(self, term):
+        '''
+        example plugin
+        '''
+        result = ''
+        term = term.strip()
+        if term == '':
+            return "Please specify your term"
+
+        if term != 'foo':
+            result = "I don't understand..."
+        else:
+            result = "bla"
+
+        return self.strip_tags(result)
+
