d5e176ce3aefe0ffd930b31ec241c02d1f42e086
martin <martin@martin-laptop.(none)>
2009-09-12 12:22:52 +0200

    - small change in setup.py - added baas.conf?

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1.1.3d'
+version = '0.1.2a.1'
 
 setup(name='baas',
       version=version,
       description="'Buddy as a Service' is a xmpp / wavelet robot using Yahoo Boss API, Google API and other services to do some stuff for you.",
       long_description="""\
+see http://mborho.github.com/baas for more infos
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python :: 2.5",
+        "Topic :: Communications :: Chat", 
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Intended Audience :: Other Audience",
+        "Operating System :: POSIX :: Linux",
+        ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='xmpp',
       author='Martin Borho',
       author_email='martin@borho.net',
       url='http://mborho.github.com/baas',
-      license='GPLv3',
+      license='GNU General Public License (GPL)',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
