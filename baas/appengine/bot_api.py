8f59770dc81418fdb24b6c3bee55f84a6471253c
mborho <martin@borho.net>
2010-09-25 12:47:51 +0200

    added appengine http api with raw json output, searchcommand in ?term=

---
+++ baas/appengine/bot_api.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2009 Martin Borho <martin@borho.net>
+# GPL - see License.txt for details
+import logging
+import ConfigParser
+from baas.core.plugins import PluginLoader
+from baas.core.helpers import *
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+config = ConfigParser.ConfigParser()
+config.read("../../baas.cfg")
+
+pluginHnd = PluginLoader(config=config,format="raw")
+pluginHnd.load_plugins()
+pluginHnd.load_map()
+pluginHnd.load_help()
+commands= pluginHnd.commands
+
+class ApiHandler(webapp.RequestHandler):
+
+    def get(self):
+        term = self.request.get('term')
+        json_mime = self.request.get('json', True)
+        reply = ''
+        try:
+            if term and term.find(':')+1:
+                cmd,args=term.split(':',1)
+                commando_func = commands.get(cmd)
+                if commando_func:
+                    result_msg = commando_func(args)
+                    reply = result_msg
+                else:
+                    reply = '{"error":"service unknown"}'
+            elif term and term == 'help':
+                reply += '{"help":pluginHnd.help}'
+
+        except Exception, e:
+            logging.exception(e)
+        
+        if json_mime:
+            self.response.headers['Content-Type'] = 'application/json charset=utf-8'
+        self.response.out.write(reply)
+
+application = webapp.WSGIApplication([('/api',ApiHandler)],debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
